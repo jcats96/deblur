1d979a80c3cc78181d611c00b825050b98e4b8cb
jtk5573 <jtk5573@rit.edu>
2019-03-12 23:08:49 -0400

    an outline of what's next

--- main.py
+++ main.py
@@ -6,6 +6,16 @@ import cv2
 
 # Load an image
 img = cv2.imread('lena.jpg')
+
+#determine blur angle using Gabor filter
+
+#determine blur length
+
+#compute point spread function
+
+#reconstruct image using weiner filter
+
+
 cv2.imshow('image',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
