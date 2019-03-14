af0b59f3aa6238305989555c46150a465687dc35
jtk5573 <jtk5573@rit.edu>
2019-03-13 23:22:16 -0400

    figuring

--- main.py
+++ main.py
@@ -7,17 +7,25 @@ import cv2
 # Load an image
 img = cv2.imread('lena.jpg')
 
-
+(img_rows,img_cols,img_channels) = img.shape
+print('rows '+ str(img_rows))
+print('cols '+ str(img_cols))
 
 #determine blur angle using Gabor filter
 theta = 0
 #determine blur length using radial basis function network - train.py in other file?
-blength = 2
+blength = 10
 #compute point spread function
 
-PSF =  np.zeros((256, 256, 1), dtype = "uint8")
+PSF =  np.zeros((img_rows, img_cols, 1), dtype = "uint8")
 
-PSF[100,100] = 255;
+PSF[img_rows//2,img_cols//2] = 255/blength
+
+# create a list of points with float coordinates along the length of theta
+
+#bilinearly interpolate to place them in the PSF
+
+
 
 #reconstruct image using weiner filter
 
