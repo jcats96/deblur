5789cc00a7cc66f5ec6643e06bf2fc2b9534b4b6
jtk5573 <jtk5573@rit.edu>
2019-03-13 23:55:04 -0400

    basic, crappy PSF from theta and length

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ print('test')
 
 import numpy as np
 import cv2
+import math
 
 # Load an image
 img = cv2.imread('lena.jpg')
@@ -11,21 +12,40 @@ img = cv2.imread('lena.jpg')
 print('rows '+ str(img_rows))
 print('cols '+ str(img_cols))
 
+
+
+
 #determine blur angle using Gabor filter
-theta = 0
+
+#im using degrees instead of radians and im not going to convert by hand because i dont have to
+theta = math.radians(45)
 #determine blur length using radial basis function network - train.py in other file?
 blength = 10
 #compute point spread function
 
 PSF =  np.zeros((img_rows, img_cols, 1), dtype = "uint8")
 
-PSF[img_rows//2,img_cols//2] = 255/blength
+#PSF[img_rows//2,img_cols//2] = 255/blength
 
 # create a list of points with float coordinates along the length of theta
+#arbitrary position starting at "0"
+#theta = 0 deg is up
+# theta = 90 deg is right
+x_coords = [0]
+y_coords = [0]
+for  hyp in range(1,10):
+    x_coords.append(hyp*math.sin(theta))
+    y_coords.append(hyp*math.cos(theta))
 
-#bilinearly interpolate to place them in the PSF
+print(x_coords)
+print(y_coords)
 
+#bilinearly interpolate, or something to place them in the PSF
 
+for  index in range(0,10):
+    point_row = (img_rows//2) + (x_coords[index])//1
+    point_col = (img_cols // 2) + (y_coords[index]) // 1
+    PSF[int(point_row),int(point_col)] += 255//blength
 
 #reconstruct image using weiner filter
 
