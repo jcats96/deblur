c7f0ab37adca1249164c32888cd2f5103e92e341
jtk5573 <jtk5573@rit.edu>
2019-03-25 17:15:23 -0400

    weiner filter kind of works

--- blur.py
+++ blur.py
@@ -3,16 +3,6 @@ import numpy as np
 import cv2
 import math
 
-
-def calcPSF(filter_rows,filter_cols, len,theta):
-    h = np.zeros((img_rows, img_cols, 1),float)
-    cv2.ellipse(h, (filter_rows // 2, filter_cols // 2), (0,len // 2), 90 - theta, 0, 360, 255, cv2.FILLED)
-
-    return h
-
-
-
-
 # Load an image
 img = cv2.imread('lenna_color.tif',0).astype('float32')
 
@@ -22,48 +12,43 @@ print('rows '+ str(img_rows))
 print('cols '+ str(img_cols))
 
 #deg to rad
-theta = 45
+theta = 60
 blength = 30
 
 #compute point spread function
 
-#PSF = calcPSF(img_rows,img_cols,blength,theta)
+PSF =  np.zeros((img_rows, img_cols, 1), float)
 
-#PSF = PSF.astype(float)
+cv2.ellipse(PSF,(int(img_rows/2),int(img_cols/2)),(0,int(blength/2)),int(90-theta),0,360,1,2)
+#normalize PSF
+print(cv2.sumElems(PSF))
+sum = cv2.sumElems(PSF)[0]
+reciprocal = 1/sum
+PSF = PSF*reciprocal
+print('psf done')
 
-# use a kernel
-
-blursize = 10
-
-kern = np.zeros((blursize, blursize, 1),float)
-for i in range(blursize):
-    kern[i,i] = 1/blursize
-
-
-blur = cv2.filter2D(img,-1,kern)
-
-#or use an FFT?
-
+blur = cv2.filter2D(img,-1,PSF)
 
 #PSF_F =  np.fft.fft2(PSF)
 
-#img_F = np.fft.fftshift(np.fft.fft2(img))
+img_F = np.fft.fftshift(np.fft.fft2(img))
 
 #
-#img_F_mag = 10*np.log(np.abs(img_F))
+img_F_mag = 10*np.log(np.abs(img_F))
 #
 #
 #dst_F = img_F*PSF_F
 
 ##dst = np.fft.ifft2(np.fft.ifftshift(dst_F))
 
-#cv2.imshow('PSF',np.uint8(PSF))
+PSF = PSF *255*100
+cv2.imshow('PSF',PSF.astype(np.uint8))
 cv2.imshow('img',np.uint8(img))
 #cv2.imshow('PSF_F',np.uint8(PSF_F))
-#cv2.imshow('img_F',np.uint8(img_F_mag))
+cv2.imshow('img_F',np.uint8(img_F_mag))
 cv2.imshow('filtered',np.uint8(blur))
 cv2.imwrite('blurred.png',blur)
-
+cv2.imwrite('PSF.png',PSF)
 
 
 cv2.waitKey(0)
--- main.py
+++ main.py
@@ -4,45 +4,75 @@ import numpy as np
 import cv2
 import math
 # Load an image as greyscale
-img = cv2.imread('lenna_color.tif',0)
+img = cv2.imread('blurred.png',0)
 (img_rows,img_cols) = img.shape
 print('rows '+ str(img_rows))
 print('cols '+ str(img_cols))
 
 img_FFT = np.fft.fft2(img)
-
-
+img_FFT = np.fft.fftshift(img_FFT)
+magnitude_spectrum = 10*np.log(np.abs(img_FFT))
 #determine blur angle using Gabor filter
 
-theta = 90
+theta =   60
 #determine blur length using radial basis function network
-blength = 20
+blength = 30
 
 
 #compute point spread function
-PSF =  np.zeros((img_rows, img_cols, 1), float)
-cv2.ellipse(PSF,(int(img_rows/2),int(img_cols/2)),(0,int(blength/2)),int(90-theta),0,360,1,2)
+PSF =  np.zeros((img_rows, img_cols), float)
+
+cv2.ellipse(PSF,(int(img_rows/2),int(img_cols/2)),(0,int(blength/2)),int(90-theta),0,360,255,2)
 #normalize PSF
 print(cv2.sumElems(PSF))
 sum = cv2.sumElems(PSF)[0]
 reciprocal = 1/sum
 PSF = PSF*reciprocal
-
+PSF = PSF *1
+print('psf done')
+print(cv2.sumElems(PSF)[0])
+#PSF = PSF *255
 #create weiner filter
 
-PSF_shifted = np.fft.fftshift(PSF)
-PSF_fft = np.fft.fft2(PSF_shifted)
-H_real = np.real(PSF_fft)
-
-weiner_filter = np.conj(PSF_fft)/(cv2.pow(H_real,2)+0.0001)
+PSF_fft = np.fft.fft2(PSF)
+H_squared = np.power(PSF_fft,2)
+k = 0.001
+weiner_filter = np.divide(np.conj(PSF_fft),H_squared+k)
 #reconstruct image using weiner filter
+print('weiner filter shape')
+print(weiner_filter.shape)
+
+weiner_filter = np.fft.fftshift(weiner_filter)
+
+output_img_fft = img_FFT.copy()
+
+for rows in range(img_rows):
+    for cols in range(img_cols):
+        output_img_fft[rows,cols] =weiner_filter[rows,cols]*img_FFT[rows,cols]
 
 
-#output_img = np.fft.ifft2(weiner_filter*img_FFT)
+output_img_fft = np.fft.ifftshift(output_img_fft)
+output_img = np.fft.ifft2(output_img_fft)
+
+output_img = np.fft.fftshift(output_img)
+
+output_img = np.real(output_img)
+
+print(output_img.shape)
+print(output_img.dtype)
+
+mag_out = 10*np.log(np.abs(output_img_fft))
+
+mag_PSF = 10*np.log(np.abs(PSF_fft))
+cv2.imshow('img',img.astype(np.uint8))
 
 
-PSF = PSF *255*100
 cv2.imshow('PSF',PSF.astype(np.uint8))
-#.imshow('Output',output_img.astype(np.uint8))
+
+cv2.imshow('img_fft',magnitude_spectrum.astype(np.uint8))
+cv2.imshow('Output FFT',mag_out.astype(np.uint8))
+cv2.imshow('magpsf',mag_PSF.astype(np.uint8))
+cv2.imshow('Output',output_img.astype(np.uint8))
+cv2.imwrite('Output.png',output_img.astype(np.uint8))
 cv2.waitKey(0)
 cv2.destroyAllWindows()
