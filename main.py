ba40fc460581c07d238cad9f2ae6fb69d46500bc
jtk5573 <jtk5573@rit.edu>
2019-03-25 09:45:49 -0400

    weiner filter in progress

--- main.py
+++ main.py
@@ -1,56 +1,48 @@
 #main
 print('test')
-
 import numpy as np
 import cv2
 import math
-
 # Load an image as greyscale
-img = cv2.imread('lena.jpg',0)
-
-
+img = cv2.imread('lenna_color.tif',0)
 (img_rows,img_cols) = img.shape
 print('rows '+ str(img_rows))
 print('cols '+ str(img_cols))
 
-
+img_FFT = np.fft.fft2(img)
 
 
 #determine blur angle using Gabor filter
 
-#im using degrees instead of radians and im not going to convert by hand because i dont have to
-theta = math.radians(45)
-#determine blur length using radial basis function network - train.py in other file?
-blength = 10
+theta = 90
+#determine blur length using radial basis function network
+blength = 20
+
+
 #compute point spread function
+PSF =  np.zeros((img_rows, img_cols, 1), float)
+cv2.ellipse(PSF,(int(img_rows/2),int(img_cols/2)),(0,int(blength/2)),int(90-theta),0,360,1,2)
+#normalize PSF
+print(cv2.sumElems(PSF))
+sum = cv2.sumElems(PSF)[0]
+reciprocal = 1/sum
+PSF = PSF*reciprocal
 
-PSF =  np.zeros((img_rows, img_cols, 1), dtype = "uint8")
+#create weiner filter
 
-#PSF[img_rows//2,img_cols//2] = 255/blength
+PSF_shifted = np.fft.fftshift(PSF)
+PSF_fft = np.fft.fft2(PSF_shifted)
+H_real = np.real(PSF_fft)
 
-# create a list of points with float coordinates along the length of theta
-#arbitrary position starting at "0"
-#theta = 0 deg is up
-# theta = 90 deg is right
-x_coords = [0]
-y_coords = [0]
-for  hyp in range(1,10):
-    x_coords.append(hyp*math.sin(theta))
-    y_coords.append(hyp*math.cos(theta))
-
-print(x_coords)
-print(y_coords)
-
-#bilinearly interpolate, or something to place them in the PSF
-
-for  index in range(0,10):
-    point_row = (img_rows//2) + (x_coords[index])//1
-    point_col = (img_cols // 2) + (y_coords[index]) // 1
-    PSF[int(point_row),int(point_col)] += 255//blength
-
+weiner_filter = np.conj(PSF_fft)/(cv2.pow(H_real,2)+0.0001)
 #reconstruct image using weiner filter
 
 
-cv2.imshow('PSF',PSF)
+#output_img = np.fft.ifft2(weiner_filter*img_FFT)
+
+
+PSF = PSF *255*100
+cv2.imshow('PSF',PSF.astype(np.uint8))
+#.imshow('Output',output_img.astype(np.uint8))
 cv2.waitKey(0)
 cv2.destroyAllWindows()
